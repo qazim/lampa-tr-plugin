f764b312229f3f82f757bffafb46f396e4621551
Qalib <ustaqalib@mail.ru>
2026-01-19 16:42:08 +0400

    Refactor Lampa plugin for cleaner structure

--- index.js
+++ index.js
@@ -1,72 +1,61 @@
 (function () {
     'use strict';
 
-    if (typeof Lampa == 'undefined') return;
+    Lampa.Listener.follow('app', function (e) {
+        if (e.type !== 'ready') return;
 
-    var BASE_URL = 'https://sinemaizle.org';
+        var BASE_URL = 'https://sinemaizle.org';
 
-    function parse(html) {
-        return Lampa.Utils.parseHtml(html);
-    }
-
-    function request(url, success, error) {
-        Lampa.Network.get(url, success, error);
-    }
-
-    Lampa.Source.Online.add({
-        id: 'sinemaizle',
-        name: 'Sinemaizle',
-        type: 'movie',
-
-        search: function (query, callback) {
-            request(BASE_URL + '/?s=' + encodeURIComponent(query), function (html) {
-                var doc = parse(html);
-                var items = [];
-
-                doc.find('article a').each(function () {
-                    var a = this;
-                    var img = a.querySelector('img');
-                    if (!img) return;
-
-                    items.push({
-                        title: img.alt || a.title || 'Sinemaizle',
-                        poster: img.src,
-                        url: a.href,
-                        type: 'movie'
-                    });
-                });
-
-                callback(items);
-            }, function () {
-                callback([]);
-            });
-        },
-
-        detail: function (url, callback) {
-            request(url, function (html) {
-                var doc = parse(html);
-                var iframe = doc.find('iframe').get(0);
-
-                if (!iframe) {
-                    callback([]);
-                    return;
-                }
-
-                callback([{
-                    title: 'Sinemaizle',
-                    url: iframe.src,
-                    quality: 'HD'
-                }]);
-            }, function () {
-                callback([]);
-            });
+        function parse(html) {
+            return Lampa.Utils.parseHtml(html);
         }
-    });
 
-    Lampa.Plugin.add({
-        name: 'Sinemaizle',
-        author: 'qazim',
-        version: '1.0'
-    });
+        Lampa.Source.Online.add({
+            id: 'sinemaizle',
+            name: 'Sinemaizle',
+            type: 'movie',
 
+            search: function (query, callback) {
+                Lampa.Network.get(BASE_URL + '/?s=' + encodeURIComponent(query), function (html) {
+                    var doc = parse(html);
+                    var items = [];
+
+                    doc.find('article').each(function () {
+                        var a = this.querySelector('a');
+                        var img = this.querySelector('img');
+                        if (!a || !img) return;
+
+                        items.push({
+                            title: img.alt,
+                            poster: img.src,
+                            url: a.href,
+                            type: 'movie'
+                        });
+                    });
+
+                    callback(items);
+                }, function () {
+                    callback([]);
+                });
+            },
+
+            detail: function (url, callback) {
+                Lampa.Network.get(url, function (html) {
+                    var doc = parse(html);
+                    var iframe = doc.find('iframe').get(0);
+
+                    if (!iframe) {
+                        callback([]);
+                        return;
+                    }
+
+                    callback([{
+                        title: 'Sinemaizle',
+                        url: iframe.src,
+                        quality: 'HD'
+                    }]);
+                });
+            }
+        });
+    });
 })();
