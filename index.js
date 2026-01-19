284c863eb12e0703541dc6e88d9ec50b2562629b
Qalib <ustaqalib@mail.ru>
2026-01-19 17:04:28 +0400

    Update film search and player handling
    Refactor searchFilm and detail functions to return search URL and open WebView.

--- index.js
+++ index.js
@@ -6,60 +6,26 @@
 
         var BASE = 'https://sinemaizle.org';
 
-        function parse(html) {
-            return Lampa.Utils.parseHtml(html);
-        }
-
         function searchFilm(object, callback) {
             var title = object.original_title || object.title;
+            if (!title) return callback([]);
 
-            if (!title) {
-                callback([]);
-                return;
-            }
-
-            Lampa.Network.get(
-                BASE + '/?s=' + encodeURIComponent(title),
-                function (html) {
-                    var doc = parse(html);
-                    var item = doc.find('article a').get(0);
-
-                    if (!item) {
-                        callback([]);
-                        return;
-                    }
-
-                    callback([{
-                        title: title,
-                        url: item.href
-                    }]);
-                },
-                function () {
-                    callback([]);
-                }
-            );
+            // возвращаем просто страницу поиска / первого найденного фильма
+            callback([{
+                title: title,
+                url: BASE + '/?s=' + encodeURIComponent(title),
+                type: 'movie'
+            }]);
         }
 
-        function getPlayer(url, callback) {
-            Lampa.Network.get(url, function (html) {
-                var doc = parse(html);
-
-                // берём ПЕРВЫЙ iframe (рекламный + потом фильм)
-                var iframe = doc.find('iframe').get(0);
-
-                if (!iframe || !iframe.src) {
-                    callback([]);
-                    return;
-                }
-
-                callback([{
-                    title: 'Sinemaizle',
-                    url: iframe.src,
-                    quality: 'WEB'
-                }]);
-            }, function () {
-                callback([]);
-            });
+        function openWebView(item, callback) {
+            callback([{
+                title: item.title,
+                url: item.url,
+                quality: 'WEB',
+                // WebView активируем через встроенный метод Lampa
+                external: true
+            }]);
         }
 
         Lampa.Source.Online.add({
@@ -72,12 +38,12 @@
             },
 
             detail: function (item, callback) {
-                getPlayer(item.url, callback);
+                openWebView(item, callback);
             }
         });
 
         Lampa.Plugin.add({
-            name: 'Sinemaizle',
+            name: 'Sinemaizle WebView',
             version: '1.0',
             author: 'qazim'
         });
