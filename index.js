b4f9ad1378a89101516656b722cbc637af6b3bab
Qalib <ustaqalib@mail.ru>
2026-01-19 15:36:15 +0400

    Refactor SinemaIzle plugin with version update
    Updated plugin version and improved parsing logic.

--- index.js
+++ index.js
@@ -1,189 +1,244 @@
 (function() {
     'use strict';
 
-    // Название плагина
     var plugin_name = 'SinemaIzle';
-    
-    // Конфигурация
-    var manifest = {
-        type: 'video',
-        version: '1.0.0',
-        name: plugin_name,
-        description: 'Поиск контента на sinemaizle.org',
-        component: 'sinemaizle'
-    };
+    var base_url = 'https://sinemaizle.org';
 
-    // Основной класс плагина
-    function Component(object) {
-        var network = new Lampa.Reguest();
-        var scroll = new Lampa.Scroll({
-            mask: true,
-            over: true
-        });
-        var items = [];
-        var html = $('<div></div>');
-        var active = 0;
-        var base_url = 'https://sinemaizle.org';
+    function startPlugin() {
+        var manifest = {
+            type: 'video',
+            version: '1.0.1',
+            name: plugin_name,
+            description: 'Онлайн просмотр с sinemaizle.org',
+            component: 'sinemaizle'
+        };
 
-        // Парсинг поисковых результатов
-        this.parseSearch = function(str) {
-            items = [];
-            var parser = new DOMParser();
-            var doc = parser.parseFromString(str, 'text/html');
+        Lampa.Manifest.plugins = manifest;
+
+        function Component(object) {
+            var network = new Lampa.Reguest();
+            var scroll = new Lampa.Scroll({
+                mask: true,
+                over: true
+            });
             
-            // Здесь нужно адаптировать селекторы под структуру сайта
-            var results = doc.querySelectorAll('.movie-item, .film-item, article'); 
-            
-            results.forEach(function(item) {
+            var items = [];
+            var html = $('<div></div>');
+            var active = 0;
+            var _this = this;
+
+            this.create = function() {
+                this.activity.loader(true);
+                
+                return this.render();
+            };
+
+            this.search = function(object, query) {
+                var _this = this;
+                
+                this.activity.loader(true);
+                
+                // Используем прокси для обхода CORS
+                var search_url = 'https://cors-anywhere.herokuapp.com/' + base_url + '/?s=' + encodeURIComponent(query);
+                
+                network.silent(search_url, this.parseSearchResults.bind(this), function(a, c) {
+                    _this.empty(Lampa.Lang.translate('torrent_parser_request_error'));
+                });
+            };
+
+            this.parseSearchResults = function(str) {
+                var _this = this;
+                
+                this.activity.loader(false);
+                
+                items = [];
+                
                 try {
-                    var link = item.querySelector('a');
-                    var img = item.querySelector('img');
-                    var title = item.querySelector('.title, h2, h3');
+                    var parser = new DOMParser();
+                    var doc = parser.parseFromString(str, 'text/html');
                     
-                    if (link && title) {
-                        items.push({
-                            title: title.textContent.trim(),
-                            url: link.href.indexOf('http') === 0 ? link.href : base_url + link.getAttribute('href'),
-                            img: img ? (img.src.indexOf('http') === 0 ? img.src : base_url + img.src) : '',
-                            quality: item.querySelector('.quality') ? item.querySelector('.quality').textContent : '',
-                            year: item.querySelector('.year') ? item.querySelector('.year').textContent : ''
-                        });
+                    // Адаптируйте селекторы под структуру сайта
+                    var results = doc.querySelectorAll('article, .movie-item, .post');
+                    
+                    results.forEach(function(item, index) {
+                        try {
+                            var link = item.querySelector('a');
+                            var title_elem = item.querySelector('h2, h3, .title, .entry-title');
+                            var img = item.querySelector('img');
+                            
+                            if (link && title_elem) {
+                                var url = link.href;
+                                if (url.indexOf('http') !== 0) {
+                                    url = base_url + url;
+                                }
+                                
+                                var img_src = '';
+                                if (img) {
+                                    img_src = img.src || img.getAttribute('data-src') || '';
+                                    if (img_src && img_src.indexOf('http') !== 0) {
+                                        img_src = base_url + img_src;
+                                    }
+                                }
+                                
+                                items.push({
+                                    title: title_elem.textContent.trim(),
+                                    url: url,
+                                    img: img_src,
+                                    quality: '',
+                                    year: ''
+                                });
+                            }
+                        } catch(e) {
+                            console.log('SinemaIzle parse error:', e);
+                        }
+                    });
+                    
+                    this.build();
+                    
+                    if (items.length === 0) {
+                        this.empty();
                     }
                 } catch(e) {
-                    console.log('Ошибка парсинга элемента:', e);
+                    console.log('SinemaIzle error:', e);
+                    this.empty();
                 }
-            });
-        };
+            };
 
-        // Поиск
-        this.search = function(query) {
-            var _this = this;
-            
-            network.silent(base_url + '/search/' + encodeURIComponent(query), function(str) {
-                _this.parseSearch(str);
-                _this.build();
-            }, function(error) {
-                Lampa.Noty.show('Ошибка поиска: ' + error);
-            });
-        };
-
-        // Построение интерфейса
-        this.build = function() {
-            var _this = this;
-            
-            scroll.render().addClass('torrent-list');
-            
-            items.forEach(function(element) {
-                var item = Lampa.Template.get('card', {
-                    title: element.title,
-                    release_year: element.year
-                });
+            this.build = function() {
+                var _this = this;
                 
-                item.addClass('card--collection');
+                scroll.render().addClass('torrent-list');
                 
-                var img = item.find('.card__img')[0];
-                img.onload = function() {
-                    item.addClass('card--loaded');
-                };
-                img.onerror = function() {
-                    img.src = './img/img_broken.svg';
-                };
-                img.src = element.img;
-                
-                item.on('hover:focus', function() {
-                    active = items.indexOf(element);
-                    scroll.update(item, true);
-                });
-                
-                item.on('hover:enter', function() {
-                    _this.openMovie(element);
-                });
-                
-                scroll.append(item);
-            });
-            
-            if (items.length === 0) {
-                scroll.append(Lampa.Template.get('list_empty'));
-            }
-            
-            scroll.append($('<div class="torrent-list__footer"><div>'));
-            html.append(scroll.render());
-        };
-
-        // Открытие фильма
-        this.openMovie = function(element) {
-            network.silent(element.url, function(str) {
-                var parser = new DOMParser();
-                var doc = parser.parseFromString(str, 'text/html');
-                
-                // Поиск видео плеера (нужно адаптировать под конкретный сайт)
-                var iframe = doc.querySelector('iframe[src*="player"], iframe[src*="video"]');
-                var video = doc.querySelector('video source');
-                
-                var video_url = '';
-                if (iframe) {
-                    video_url = iframe.src;
-                } else if (video) {
-                    video_url = video.src;
-                }
-                
-                if (video_url) {
-                    Lampa.Player.play({
+                items.forEach(function(element) {
+                    var item = Lampa.Template.get('card', {
                         title: element.title,
-                        url: video_url
+                        release_year: element.year
                     });
-                } else {
-                    Lampa.Noty.show('Не удалось найти видео');
-                }
-            }, function(error) {
-                Lampa.Noty.show('Ошибка загрузки: ' + error);
-            });
-        };
+                    
+                    item.addClass('card--collection');
+                    
+                    var img = item.find('.card__img')[0];
+                    if (img) {
+                        img.onload = function() {
+                            item.addClass('card--loaded');
+                        };
+                        img.onerror = function(e) {
+                            img.src = './img/img_broken.svg';
+                        };
+                        if (element.img) {
+                            img.src = element.img;
+                        }
+                    }
+                    
+                    item.on('hover:focus', function() {
+                        active = items.indexOf(element);
+                        scroll.update(item, true);
+                    });
+                    
+                    item.on('hover:enter', function() {
+                        _this.openMovie(element);
+                    });
+                    
+                    scroll.append(item);
+                });
+                
+                scroll.append($('<div class="torrent-list__footer"></div>'));
+            };
 
-        // Инициализация
-        this.start = function() {
-            if (Lampa.Activity.active().activity !== this.activity) return;
-            this.search(object.search);
-        };
+            this.openMovie = function(element) {
+                var _this = this;
+                
+                Lampa.Activity.loading(true);
+                
+                var movie_url = 'https://cors-anywhere.herokuapp.com/' + element.url;
+                
+                network.silent(movie_url, function(str) {
+                    Lampa.Activity.loading(false);
+                    
+                    try {
+                        var parser = new DOMParser();
+                        var doc = parser.parseFromString(str, 'text/html');
+                        
+                        var iframe = doc.querySelector('iframe[src*="player"], iframe[src*="video"], iframe[src*="embed"]');
+                        var video = doc.querySelector('video source, video');
+                        
+                        var video_url = '';
+                        
+                        if (iframe) {
+                            video_url = iframe.src || iframe.getAttribute('data-src');
+                        } else if (video) {
+                            video_url = video.src || video.querySelector('source')?.src;
+                        }
+                        
+                        if (video_url) {
+                            Lampa.Player.play({
+                                title: element.title,
+                                url: video_url
+                            });
+                            
+                            Lampa.Player.playlist([{
+                                title: element.title,
+                                url: video_url
+                            }]);
+                        } else {
+                            Lampa.Noty.show('Видео не найдено');
+                        }
+                    } catch(e) {
+                        console.log('SinemaIzle player error:', e);
+                        Lampa.Noty.show('Ошибка загрузки видео');
+                    }
+                }, function(a, c) {
+                    Lampa.Activity.loading(false);
+                    Lampa.Noty.show('Ошибка загрузки страницы');
+                });
+            };
 
-        this.pause = function() {};
-        this.stop = function() {};
-        this.render = function() {
-            return html;
-        };
-        this.destroy = function() {
-            network.clear();
-            scroll.destroy();
-            html.remove();
-        };
-    }
+            this.empty = function(msg) {
+                var empty = Lampa.Template.get('list_empty');
+                if (msg) empty.find('.empty__descr').text(msg);
+                scroll.render().addClass('torrent-list');
+                scroll.append(empty);
+                this.loading(false);
+            };
 
-    // Регистрация компонента
-    Lampa.Component.add('sinemaizle', Component);
+            this.start = function() {
+                if (Lampa.Activity.active().activity !== this.activity) return;
+                
+                this.search(object, object.search);
+            };
 
-    // Добавление в меню поиска
-    Lampa.Manifest.plugins = manifest;
-    
-    // Регистрация источника онлайн контента
-    function startPlugin() {
-        window.plugin_sinemaizle_ready = true;
+            this.pause = function() {};
+            
+            this.stop = function() {};
+            
+            this.render = function() {
+                return html;
+            };
+            
+            this.destroy = function() {
+                network.clear();
+                scroll.destroy();
+                html.remove();
+            };
+        }
+
+        Lampa.Component.add('sinemaizle', Component);
         
+        // Добавляем в список онлайн источников
         Lampa.Template.add('sinemaizle_style', '<style>.sinemaizle-plugin{padding:1em}</style>');
         $('body').append(Lampa.Template.get('sinemaizle_style', {}, true));
         
-        // Добавление в список онлайн источников
-        if (Lampa.Arrays && Lampa.Arrays.onlines) {
-            Lampa.Arrays.onlines.push({
-                title: 'SinemaIzle',
-                component: 'sinemaizle'
-            });
-        }
+        console.log('SinemaIzle plugin loaded successfully');
+        Lampa.Noty.show('Плагин SinemaIzle загружен');
     }
 
-    if (window.appready) startPlugin();
-    else {
+    if (window.appready) {
+        startPlugin();
+    } else {
         Lampa.Listener.follow('app', function(e) {
-            if (e.type === 'ready') startPlugin();
+            if (e.type === 'ready') {
+                startPlugin();
+            }
         });
     }
 
